6daf83724007106d6d72508ae66b245ce61d21f9
ThiziriCHEBILI <sahel.thiziri4@gmail.com>
2026-07-24 14:46:50 +0200

    Création et style du composant Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,15 @@
+import logo from "../../assets/Logo-footer.png";
+import "./Footer.scss";
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer className="footer">
+      <img src={logo} alt="Logo Kasa" className="footer__logo" />
+      <p className="footer__text">© 2020 Kasa. All rights reserved</p>
+
+    </footer>
+    
   );
 }
 
 export default Footer;
+
