1fe480409d1d763687ea94af37fc7cb5b600c052
ThiziriCHEBILI <sahel.thiziri4@gmail.com>
2026-07-24 13:56:10 +0200

    Style du composant Card, mise en grille des logements

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,15 +3,19 @@ import Banner from "../../components/Banner/Banner";
 import Card from "../../components/Card/Card";
 import logements from "../../data/logements.json";
 import imagehome from "../../assets/image-home.png";
+import "./Home.scss";
+
 function Home() {
   return (
     <div className="container">
       <Banner image={imagehome} texte="Chez vous, partout et ailleurs" />
+      <section className="card-container">
       {logements.map((logement) => (
         <Link key={logement.id} to={`/logement/${logement.id}`}>
           <Card title={logement.title} cover={logement.cover} />
         </Link>
       ))}
+      </section>
     </div>
   );
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,12 @@
+import "./Card.scss";
 function Card(props) {
   return (
-    <article>
-      <img src={props.cover} alt="Carte" />
-      <p>{props.title}</p>
+    
+    <article className="card"> 
+      <img src={props.cover} alt="Carte" className="card__img" />
+      <h2 className="card__title">{props.title}</h2>
     </article>
+    
   );
 }
 export default Card;
