ca9e1213b1144cca233b8a3cbd8b669448936773
ThiziriCHEBILI <sahel.thiziri4@gmail.com>
2026-07-28 14:11:00 +0200

    About.jsx fonctionnel : Banner et 4 Collapse générés via map()

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,12 +1,40 @@
 import Collapse from "../../components/Collapse/Collapse";
+import Banner from "../../components/Banner/Banner";
+import imageapropos from "../../assets/image-apropos.png";
 
 function About() {
+  const apropos = [
+    {
+      title: "Fiabilité",
+      content:
+        "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.",
+    },
+    {
+      title: "Respect",
+      content:
+        "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
+    },
+    {
+      title: "Service",
+      content:
+        "La qualité du service est au cœur de notre engagement chez Kasa. Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance.",
+    },
+    {
+      title: "Sécurité",
+      content:
+        "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+    },
+  ];
   return (
-    <div>
-      <Collapse title="Fiabilité" content="Texte de test pour vérifier que ça marche" />
+    <div className="apropos__container">
+      <Banner image={imageapropos} />
+      <div className="collapse__container">
+        {apropos.map((item, index) => (
+          <Collapse key={index} title={item.title} content={item.content} />
+        ))}
+      </div>
     </div>
   );
 }
 
 export default About;
-
