b173cf125490160d7aeebea487fb5293eed003f9
ThiziriCHEBILI <sahel.thiziri4@gmail.com>
2026-07-28 13:16:52 +0200

    Création du composant Collapse fonctionnel (utilisé dans About)

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,7 +1,12 @@
+import Collapse from "../../components/Collapse/Collapse";
+
 function About() {
   return (
-    <div>About</div>
+    <div>
+      <Collapse title="Fiabilité" content="Texte de test pour vérifier que ça marche" />
+    </div>
   );
 }
 
 export default About;
+
---
+++ src/components/Collapse/Collapse.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import Arrow from "../../assets/arrow.svg";
+
+function Collapse({ title, content }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="collapse">
+      <button className="collapse__button">
+        {title}
+        <img
+          src={Arrow}
+          alt="Arrow"
+          className={`collapse__arrow ${isOpen ? "collapse__arrow--open" : ""}`}
+          onClick={() => setIsOpen(!isOpen)}
+        />
+      </button>
+      {isOpen && <div className="collapse__content">{content}</div>}
+    </div>
+  );
+}
+
+export default Collapse;
