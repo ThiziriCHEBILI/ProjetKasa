7a7f8eb0096a6c1704e8c3f5e75bc234c7fcc356
ThiziriCHEBILI <sahel.thiziri4@gmail.com>
2026-07-31 11:35:40 +0200

    Creation du composant Slideshow

---
+++ kasa/src/components/Slideshow/Slideshow.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+function Slideshow({ pictures }) {
+  const [imageIndex, setImageIndex] = useState(0);
+
+  const nextImage = () => {
+    setImageIndex(imageIndex === pictures.length - 1 ? 0 : imageIndex + 1);
+  };
+
+  const previousImage = () => {
+    setImageIndex(imageIndex === 0 ? pictures.length - 1 : imageIndex - 1);
+  };
+
+  return (
+    <div>
+      <img src={pictures[imageIndex]} alt="image carousel" />
+      {pictures.length > 1 && (
+        <div>
+          <i className="fa-solid fa-angle-left" onClick={previousImage}></i>
+          <i className="fa-solid fa-angle-right" onClick={nextImage}></i>
+          {imageIndex + 1}/ {pictures.length}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Slideshow;
