a97237cbcdc1564731cf278ffe9c415f1daf6d16
ThiziriCHEBILI <sahel.thiziri4@gmail.com>
2026-08-03 16:34:06 +0200

    Style et responsive de la page Error 404

--- kasa/src/Pages/Error/Error.jsx
+++ kasa/src/Pages/Error/Error.jsx
@@ -1,7 +1,17 @@
-function Error() {
+import { Link } from "react-router-dom";
+import "./Error.scss";
+
+export default function Error() {
   return (
-    <div>Error</div>
+    <div className="error">
+      <h1 className="error__title">404</h1>
+      <p className="error__message">
+        {" "}
+        Oups! La page que <wbr /> vous demandez n'existe pas.
+      </p>
+      <Link to="/" className="error__link">
+        Retourner sur la page d'accueil
+      </Link>
+    </div>
   );
 }
-
-export default Error;
